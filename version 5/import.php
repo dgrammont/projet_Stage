8b61f86f10adef39b30c4648353a2ac50163459e
dgrammont <dylanlaudat@orange.fr>
2020-10-20 14:58:31 +0200

    Ajout de ligne pour correspondre au projet voulu , manque le rajout de ligne par l'utilisateur et le calcul de productivité

---
+++ version 5/import.php
@@ -0,0 +1,101 @@
+<?php
+
+//import.php
+
+//variable pour les id 
+
+//nombre de ligne
+$nbligne = 1;
+//heure de debut du cariste
+$time_Cariste_h_Deb="caristeHdeb";
+//heure de fin du cariste
+$time_Cariste_h_Fin="caristeHfin";
+//nom du cariste
+$nom_Cariste="nomCariste";
+//nombre de palette du Cariste
+$nb_pal_Cariste="nbPalCariste";
+
+//heure de debut du chargeur
+$time_Chargeur_h_Deb="chargeurHdeb";
+//heure de fin du chargeur
+$time_Chargeur_h_Fin="chargeurhfin";
+//nom du chargeur
+$nom_Chargeur="nomChargeur";
+//nombre de palette du chargeur
+$nb_pal_Chargeur="nbPalChargeur";
+
+
+$expMag = "EXPE MAG";
+$uploaddir = '/home/grammont/Documents/VersionFinalCsv/'; // repertoir ou sera stocke finalement le fichier
+$uploadfile = $uploaddir . basename($_FILES['csv_file']['name']);
+$data = array();
+if (move_uploaded_file($_FILES['csv_file']['tmp_name'], $uploadfile)) { // upload ok
+    if (!empty($uploadfile)) {
+        $file_data = fopen($uploadfile, 'r');
+        fgetcsv($file_data, 0, ";");
+        while ($row = fgetcsv($file_data, 0, ";")) {
+           
+           //ajoue de +1 a la fin de la chaine de caractere 
+           $destock_deb = $time_Cariste_h_Deb.$nbligne;
+           $destock_fin = $time_Cariste_h_Fin.$nbligne;
+           $destock_nom_Cariste = $nom_Cariste.$nbligne;
+           $destock_nb_pal = $nb_pal_Cariste.$nbligne;
+           
+           $charg_deb = $time_Chargeur_h_Deb.$nbligne;
+           $charg_fin = $time_Chargeur_h_Fin.$nbligne;
+           $charg_nomChargeur = $nom_Chargeur.$nbligne;
+           $charg_nb_pal = $nb_pal_Chargeur.$nbligne;
+           
+            if ($row[2] !== "") {
+                array_push($data, array(
+                    'ligne' => $nbligne,
+                    'expo_Mag' => $expMag,
+                    'report' => "<input type='text'style='width: 40px; height: 10px'/>" ,
+                    'dpt' => utf8_encode($row[3]),
+                    'hrev' => utf8_encode($row[5]),
+                    'transporteur' => utf8_encode($row[7]),
+                    'edi' => utf8_encode($row[8]),
+                    'destinataires' => utf8_encode($row[11]),
+                    'heure_Liv' => utf8_encode($row[13]),
+                    'nb_Supp' => utf8_encode($row[14]),
+                    'quai' => "<input type='text'/>",
+                    'cariste' => "<input type='text' id='$destock_nom_Cariste'/>",
+                    'destock_HD' => "<input type='time' id='$destock_deb' onchange='ampm()'/>",
+                    'destock_HF' => "<input type='time' id='$destock_fin' onchange='ampm()'/>",
+                    'h_Arriv' => "<input type='time'/>",
+                    'porte' => "<input type='text'/>",
+                    'chargeur' => "<input type='text' id='$charg_nomChargeur'/>",
+                    'charg_H_Deb' => "<input type='time' id='$charg_deb' onchange='ampm()'/>",
+                    'charg_H_Fin' => "<input type='time' id='$charg_fin' onchange='ampm()'/>",
+                    'nb_Supp_Charg' => "<input type='number' id='$charg_nb_pal' style='width: 100px'/>",
+                    'nb_Raq' => "<input type='number' style='width: 100px'/>",
+                    'nb_pal_leg' => "<input type='number' style='width: 100px'/>",
+                    'site' => "<input type='text'/>",
+                ));
+                $nbligne = $nbligne + 1;
+                
+            }
+        }
+    }
+} else { // pb upload
+    $data[] = array(
+        'numero_De_Dossier' => "pb upload",
+        'ville_Enl' => "pb upload",
+        'cp_Liv' => "pb upload",
+        'date_Enl' => "pb upload",
+        'heure_Enl' => "pb upload",
+        'date_Liv' => "pb upload",
+        'trp' => "pb upload",
+        'ref_Chargement' => "pb upload",
+        'code_Dest' => "pb upload",
+        'date_Liv' => "pb upload",
+        'destinataire' => "pb upload",
+        'ville_Liv' => "pb upload",
+        'heure_Liv' => "pb upload",
+        'palette' => "pb upload",
+        'mt_Transport' => "pb upload"
+    );
+}
+// envoyer le contenu au format json
+header('Content-Type: application/json;charset=utf-8');
+echo json_encode($data);
---
+++ version 5/index.php
@@ -0,0 +1,155 @@
+
+<?php
+//index.php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Import CSV File into Jquery Datatables using PHP Ajax</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <style>
+            .box
+            {
+                max-width:800px;
+                width:100%;
+                margin: 0 auto;;
+            }
+            
+        </style>
+    </head>
+    <body>
+        <div class="container-md" style="margin-left: 24px;margin-right: 24px;">
+            <br />
+            <h3 align="center">Import CSV File into Jquery Datatables using PHP Ajax</h3>
+            <br />
+            <form id="upload_csv" method="post" enctype="multipart/form-data">
+                <div class="col-md-3">
+                    <br />
+                </div>  
+                <div class="col-md-4">  
+                    <input type="file" name="csv_file" id="csv_file" accept=".csv" style="margin-top:15px;" />
+                </div>  
+                <div class="col-md-5">  
+                    <input type="submit" name="upload" id="upload" value="Upload" style="margin-top:10px;" class="btn btn-info" />
+                </div>  
+                <div style="clear:both"></div>
+            </form>
+            <br />
+            <br />
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered" id="data-planning">
+                    <caption>Planning final</caption>
+                    <thead>
+
+                        <tr>
+                            <th>Ligne</th>
+                            <th>Expe-Mag-Transfert-reverse</th>  
+                            <th>report</th>
+                            <th>DPT</th>
+                            <th>N°EDI</th>
+                            <th>H_REV</th>
+                            <th>Trasporteur</th>
+                            <th>H_Liv</th>
+                            <th>Destinataire</th>
+                            <th>nb_supp</th>
+                            <th>Quai</th>
+                            <th>Cariste</th>
+                            <th>Destock_H_Deb</th>
+                            <th>Destock_H_Fin</th>
+                            <th>H_Arriv</th>
+                            <th>Porte</th>
+                            <th>chargeur</th>
+                            <th>Charg_h_Deb</th>
+                            <th>Charg_h_Fin</th>
+                            <th>Nb_supp_Chargées</th>
+                            <th>Nb_RAQ</th>
+                            <th>Nb_Pal_legères</th>
+                            <th>site</th>
+                        </tr>
+
+                    </thead>
+                </table>
+            </div>
+        </div>
+        <span id="display_time"></span>
+    </body>
+</html>
+
+<script>
+
+    $(document).ready(function () {
+        $('#upload_csv').on('submit', function (event) {
+            event.preventDefault();
+            $.ajax({
+                url: "import.php",
+                method: "POST",
+                data: new FormData(this),
+                dataType: 'json',
+                contentType: false,
+                cache: false,
+                processData: false,
+                success: function (jsonData)
+                {
+                    $('#csv_file').val('');
+                    $('#data-planning').DataTable({
+                        data: jsonData,
+                        columns: [
+                            {data: "ligne"},
+                            {data: "expo_Mag"},
+                            {data: "report"},
+                            {data: "dpt"},
+                            {data: "edi"},
+                            {data: "hrev"},
+                            {data: "transporteur"},
+                            {data: "heure_Liv"},
+                            {data: "destinataires"},
+                            {data: "nb_Supp"},
+                            {data: "quai"},
+                            {data: "cariste"},
+                            {data: "destock_HD"},
+                            {data: "destock_HF"},
+                            {data: "h_Arriv"},
+                            {data: "porte"},
+                            {data: "chargeur"},
+                            {data: "charg_H_Deb"},
+                            {data: "charg_H_Fin"},
+                            {data: "nb_Supp_Charg"},
+                            {data: "nb_Raq"},
+                            {data: "nb_pal_leg"},
+                            {data: "site"}
+                        ]
+                    });
+                }
+            });
+        });
+    });
+
+</script>
+
+<script>
+    function ampm() {
+        var id = document.getElementById(id).value;
+        console.log(id);
+        if (id.value !== "") {
+            var hours = id.split(":")[0];
+            var minutes = id.split(":")[1];
+            hours = hours < 10 ? +hours : hours;
+            var displayTime = hours + ":" + minutes;
+            document.getElementById("display_time").innerHTML = displayTime;
+            if
+                    var elem = document.getElementById(id).value;
+
+            document.getElementById(id).style.backgroundColor = "#FF0000";
+        }
+    }
+
+
+
+</script>
+
+
